5d30ca0ca833c1b80badd9df53d5251387a96eff
高宗卫 <1009464715@qq.com>
2016-12-04 22:26:04 +0800

    商家

--- mobile/weixin/act.php
+++ mobile/weixin/act.php
@@ -5,28 +5,27 @@ require(dirname(__FILE__) . '/api.class.php');
 $user_id = $_SESSION['user_id'];
 if(!$user_id){   //未登录
     show_message($_LANG['require_login'], array('登录', '返回首页'), array('user.php?act=login', $ecs->url()), 'error', false);
-}else{
-  $sql = "SELECT rank_points, vip_award FROM ".
-                  $GLOBALS['ecs']->table('users') .
-                  " WHERE user_id = '$user_id' limit 0,1";
-  $user = $db->getAll($sql);
-  if($user = $user[0]){
-
-    if($user['rank_points'] < 10000000){
-        show_message("非VIP会员，</br>请先升级为VIP会员", array('升级VIP', '返回首页'), array('goods.php?id=292', $ecs->url()), 'error', false);
-    }
-
-    if($user['vip_award'] <= 0){
-        show_message("您抽奖金额已累计达到最高", array('再次购买VIP', '返回首页'), array('goods.php?id=292', $ecs->url()), 'error', false);
-    }
-
-  }else{
-    show_message($_LANG['require_login'], array('登录', '返回首页'), array('user.php?act=login', $ecs->url()), 'error', false);
-  }
-
 }
+// else{
+//   $sql = "SELECT rank_points, vip_award FROM ".
+//                   $GLOBALS['ecs']->table('users') .
+//                   " WHERE user_id = '$user_id' limit 0,1";
+//   $user = $db->getAll($sql);
+//   if($user = $user[0]){
 
+//     if($user['rank_points'] < 10000000){
+//         show_message("非VIP会员，</br>请先升级为VIP会员", array('升级VIP', '返回首页'), array('goods.php?id=292', $ecs->url()), 'error', false);
+//     }
 
+//     if($user['vip_award'] <= 0){
+//         show_message("您抽奖金额已累计达到最高", array('再次购买VIP', '返回首页'), array('goods.php?id=292', $ecs->url()), 'error', false);
+//     }
+
+//   }else{
+//     show_message($_LANG['require_login'], array('登录', '返回首页'), array('user.php?act=login', $ecs->url()), 'error', false);
+//   }
+
+// }
 
 $aid = intval($_GET['aid']);
 $act = $db->getRow ( "SELECT * FROM " . $GLOBALS['ecs']->table('weixin_act') . " WHERE `aid` = $aid and isopen=1" );
